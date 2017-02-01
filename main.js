a11357a694eeb3226eaa8955e89474b74d356b32
Derrick Sebesta <dsebesta@gmail.com>
2017-01-31 17:04:08 -0800

    initial brainstorming

---
+++ main.js
@@ -0,0 +1,16 @@
+// initial brainstorming
+
+var game_board = [
+    ['R', '', '', '', '', ''], // column 1
+    ['B', '', '', '', '', ''], // column 2
+    ['R', 'R', '', '', '', ''], // column 3
+    ['B', 'R', '', '', '', ''], // column 4
+    ['B', 'B', 'R', '', '', ''], // column 5
+    ['B', 'R', 'B', 'R', '', '']  // column 6
+];
+
+
+// Different Match Possibilities
+// 00 01 02 03
+// 00 10 20 30
+// 00 11 22 33
